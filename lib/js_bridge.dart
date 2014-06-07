a44e533439213612aaa93d96d91564986d5ed386
Jim Trainor <jim.trainor@jptrainor.com>
2014-06-07 08:02:32 -0400

    Update license comment in source title to match what I see used in the Dart SDK source.

--- lib/js_bridge.dart
+++ lib/js_bridge.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2014, Jim Trainor. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 library js_bridge;
 
 import 'dart:js' as js;
