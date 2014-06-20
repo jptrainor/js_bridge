fe7fc96a4ca9dac02819cb3bc64295707c29decf
Jim Trainor <jim.trainor@jptrainor.com>
2014-06-20 08:43:06 -0400

    fix benchmark library name

--- benchmark/benchmark.dart
+++ benchmark/benchmark.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2014, Jim Trainor. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-library js_bridge_test_main;
+library benchmark;
 
 import 'dart:async';
 import 'dart:js' as js;
--- benchmark/perf_test.dart
+++ benchmark/perf_test.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2014, Jim Trainor. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-part of js_bridge_test_main;
+part of benchmark;
 
 Future insertJsScript(String src) {
   var script = new html.ScriptElement();
