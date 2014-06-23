5be303dbf9aaab526b068290080579fe100dd1fa
Jim Trainor <jim.trainor@jptrainor.com>
2014-06-22 21:05:53 -0400

    add warmup

--- benchmark/perf_test.dart
+++ benchmark/perf_test.dart
@@ -3,64 +3,79 @@
 
 part of benchmark;
 
-Future _runBarePerfTest() {
-  const callCount = 1e6;
+class _RunBarePerfTest {
+  bool _warmup;
 
-  dynamic recv(/*ReplyHandler replyHandler,*/ dynamic msg) {
-    return msg;
-  }
+  _RunBarePerfTest(this._warmup);
 
-  var jsToDartCompleter = new Completer();
+  call() {
+    const callCount = 1e6;
 
-  jsToDartCompletionCallback(elapsedMilliSeconds) {
-    var usPerCall = (1.0e6 * elapsedMilliSeconds) ~/ callCount;
-    print("bare messaging performance, javascript call to dart = $usPerCall ns/call");
-    jsToDartCompleter.complete();
-  }
-  ;
+    dynamic recv(/*ReplyHandler replyHandler,*/ dynamic msg) {
+      return msg;
+    }
 
-  bridgeCompletionCallback(elapsedMilliSeconds) {
-    var usPerCall = (1.0e6 * elapsedMilliSeconds) ~/ callCount;
-    print("js_bridge messaging performance, javascript call to dart = $usPerCall ns/call");
-  }
-  ;
+    var jsToDartCompleter = new Completer();
 
-  js.JsObject perfTest = new js.JsObject(js.context['JsBridgePerfTest'], [recv, callCount, jsToDartCompletionCallback, bridgeCompletionCallback]);
-
-  perfTest.callMethod('runJs2Dart');
-
-  void dartToJsPerfTest(_) {
-
-    var stopWatch = new Stopwatch()..start();
-    for (int i = 0; i < callCount; i++) {
-      var result = perfTest.callMethod('dart2Js', [i]);
-      // don't want overhead of expect call in timed loop, just compare.
-      if (result != i) {
-        expect("unexpected dart2Js result", isFalse);
+    report(msg) {
+      if (!_warmup) {
+        print(msg);
       }
     }
-    stopWatch.stop();
-    var perCall = 1000.0 * stopWatch.elapsedMicroseconds ~/ callCount;
-    var units = "ns/call";
+    ;
 
-    print("bare messaging performance, dart call to javascript = $perCall $units");
+    jsToDartCompletionCallback(elapsedMilliSeconds) {
+      var usPerCall = (1.0e6 * elapsedMilliSeconds) ~/ callCount;
+      report("bare messaging performance, javascript call to dart = $usPerCall ns/call");
+      jsToDartCompleter.complete();
+    }
+    ;
+
+    bridgeCompletionCallback(elapsedMilliSeconds) {
+      var usPerCall = (1.0e6 * elapsedMilliSeconds) ~/ callCount;
+      report("js_bridge messaging performance, javascript call to dart = $usPerCall ns/call");
+    }
+    ;
+
+    js.JsObject perfTest = new js.JsObject(js.context['JsBridgePerfTest'], [recv, callCount, jsToDartCompletionCallback, bridgeCompletionCallback]);
+
+    perfTest.callMethod('runJs2Dart');
+
+    void dartToJsPerfTest(_) {
+
+      var stopWatch = new Stopwatch()..start();
+      for (int i = 0; i < callCount; i++) {
+        var result = perfTest.callMethod('dart2Js', [i]);
+        // don't want overhead of expect call in timed loop, just compare.
+        if (result != i) {
+          expect("unexpected dart2Js result", isFalse);
+        }
+      }
+      stopWatch.stop();
+      var perCall = 1000.0 * stopWatch.elapsedMicroseconds ~/ callCount;
+      var units = "ns/call";
+
+      report("bare messaging performance, dart call to javascript = $perCall $units");
+    }
+    ;
+
+    int dartBenchmarkFunc(int arg) {
+      return arg;
+    }
+    ;
+
+    void bridgePerfTest(_) {
+      var bridge = new jsb.JsBridge("benchmark");
+      bridge.registerHandler1("dartBenchmarkFunc", dartBenchmarkFunc);
+      perfTest.callMethod('runJsBridge2Dart');
+    }
+
+    return jsToDartCompleter.future.then(dartToJsPerfTest).then(bridgePerfTest);
   }
-  ;
 
-  int dartBenchmarkFunc(int arg) {
-    return arg;
-  }
-  ;
-
-  void bridgePerfTest(_) {
-    var bridge = new jsb.JsBridge("benchmark");
-    bridge.registerHandler1("dartBenchmarkFunc", dartBenchmarkFunc);
-    perfTest.callMethod('runJsBridge2Dart');
-  }
-
-  return jsToDartCompleter.future.then(dartToJsPerfTest).then(bridgePerfTest);
 }
 
 setupPerfTest() {
-  test("js_bridge perf", _runBarePerfTest);
+  test("js_bridge perf warmup", new _RunBarePerfTest(true));
+  test("js_bridge perf measure", new _RunBarePerfTest(false));
 }
